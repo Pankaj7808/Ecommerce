d6fa59433ed99dd75a64c25dbb3439af9e5d6747
taybaa <tayba3@gmail.com>
2024-12-09 22:17:26 +0530

    product

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegisterSerializer
+from rest_framework.decorators import api_view
+
 
 class RegisterAPIView(APIView):
     def post(self, request):
@@ -10,3 +12,6 @@ class RegisterAPIView(APIView):
             serializer.save()
             return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    
